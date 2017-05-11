383df8305d3c7dba77a811726abe468b69fd561e
Cici <cicichen2015@qq.com>
2017-05-11 16:36:10 +1200

    changed config for heroku

--- knexfile.js
+++ knexfile.js
@@ -9,8 +9,7 @@ module.exports = {
     pool: {
      afterCreate: (conn, cb) =>
        conn.run('PRAGMA foreign_keys = ON', cb)
-   },
-   production: process.env.DATABASE_URL
+   }
   },
 
   test: {
@@ -23,11 +22,12 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
+    // connection: {
+    //   database: 'my_db',
+    //   user: 'username',
+    //   password: 'password'
+    // },
 
     pool: {
       min:2,
