d6a3662c1981897820c1ee7411131d321b536469
cici <cici>
2017-05-15 18:51:56 +1200

    work in progress

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,10 @@ router.get('/', function(req,res) {
   res.render('home')
 })
 
+router.post('/chapter/1', function(req,res) {
+  var data = req.body
+  res.send(data)
+  // res.render('chapterOne',data)
+} )
+
 module.exports = router
