93b655a50a219186e02afa1db67b398e86622be7
cici-chen <cici-chen>
2017-06-06 16:04:01 +1200

    added client api and one passing test

--- tests/api/client-users.test.js
+++ tests/api/client-users.test.js
@@ -9,16 +9,27 @@ const userInfo={
 }
 
 var scope = nock('http://localhost:80')
+//Here you're telling nock: when I visit localhost(the port is 80 I don't know why, it doesn't matter which port your app runs on), give me those things.
   .get('/api/v1/users/1')
   .reply(200, userInfo)
-//Here you're telling nock: when I visit localhost(the port is 80 I don't know why), give me those things.
-//You're telling nock how to pretend to behave as the real server
+  //You're telling nock how to pretend to behave as the real server
+  .post('/api/v1/users', userInfo)
+  //nock allows chaining like this
+  .reply(201)
 
-test.only.cb('getUser gets info of a single user', t =>{
+test.cb('getUser gets info of a single user', t =>{
   api.getUser(1, (actual)=>{
     scope.done()
     //Here you're saying: did you get a request? If yes this will not retrun error
     t.is(actual.user_name,"Sabrina")
+    t.end()
+  })
+})
+
+test.cb('saveUser post userInfo to the route', t =>{
+  api.saveUser(userInfo,(err)=>{
+    scope.done()
+    t.is(err, null)
     t.end()
   })
 })
